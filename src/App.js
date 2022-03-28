e4f92c9830540fbdf42db30b1eb29da497c1b256
Amrit Kaur <84275645+amritkaur20@users.noreply.github.com>
2022-03-28 20:59:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./App.css";
+import GridRow from "./component/GridRow";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      boardState: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+      ],
+      turn: "x"
+    };
+  }
+  updateBoard = (row, col) => {
+    const arr = this.state.boardState;
+    arr[row][col] = this.state.turn;
+    this.setState({
+      turn: this.state.turn === "x" ? "0" : "x",
+      boardState: arr
+    });
+  };
+  render() {
+    return (
+      <>
+        <h1 className="header">Tic Tac Toe</h1>
+        <div className="container">
+          {[0, 1, 2].map((row) => {
+            return (
+              <GridRow
+                index={row}
+                boardState={this.state.boardState}
+                updateBoard={this.updateBoard}
+              />
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+}
+export default App;
